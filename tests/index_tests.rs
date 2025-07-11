2ca3ca7beac434ede5b181546460aeceec490692
Jerome Eteve <jerome.eteve@gmail.com>
2025-07-11 21:31:19 +0000

    test getting document IDs

--- tests/index_tests.rs
+++ tests/index_tests.rs
@@ -15,7 +15,9 @@ fn test_few_docs() {
     let mut index = Index::new();
     let d1 = Document::default().add_field("colour".into(), "blue".into());
     let d2 = Document::default().add_field("colour".into(), "green".into());
-    let d3 = Document::default().add_field("taste".into(), "sweet".into());
+    let d3 = Document::default()
+        .add_field("taste".into(), "sweet".into())
+        .add_field("colour".into(), "blue".into());
 
     let doc_id1 = index.index_document(d1);
     let doc_id2 = index.index_document(d2);
@@ -29,4 +31,15 @@ fn test_few_docs() {
     assert!(index.term_iter("colour", "purple").is_none());
     assert!(index.term_iter("colour", "blue").is_some());
     assert!(index.term_iter("taste", "sweet").is_some());
+
+    let sweet_docs = index
+        .term_iter("taste", "sweet")
+        .map(|iter| iter.collect::<Vec<_>>());
+
+    assert_eq!(sweet_docs, Some(vec![2]));
+
+    let blue_docs = index
+        .term_iter("colour", "blue")
+        .map(|iter| iter.collect::<Vec<_>>());
+    assert_eq!(blue_docs, Some(vec![0, 2]));
 }
