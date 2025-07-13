b3960a4f86e13ed35e18cc18bc4534d60810ad68
Jerome Eteve <jerome.eteve@gmail.com>
2025-07-13 21:30:46 +0000

    Implemented Conjunction index filter

--- src/models/queries.rs
+++ src/models/queries.rs
@@ -1,18 +1,12 @@
-use crate::models::index;
+use crate::models::index::{DocId, Index};
 
 use super::documents::Document;
 use std::rc::Rc;
 
 pub trait Query: std::fmt::Debug {
-    fn docids_from_index<'a>(
-        &self,
-        index: &'a index::Index,
-    ) -> Box<dyn Iterator<Item = index::DocId> + 'a>;
+    fn docids_from_index<'a>(&self, index: &'a Index) -> Box<dyn Iterator<Item = DocId> + 'a>;
 
-    fn docs_from_index<'a>(
-        &self,
-        index: &'a index::Index,
-    ) -> Box<dyn Iterator<Item = &'a Document> + 'a> {
+    fn docs_from_index<'a>(&self, index: &'a Index) -> Box<dyn Iterator<Item = &'a Document> + 'a> {
         Box::new(
             self.docids_from_index(index)
                 .map(|doc_id| index.get_document(doc_id).expect("Document should exist")),
@@ -37,11 +31,117 @@ impl Query for ConjunctionQuery {
         self.queries.iter().all(|q| q.matches(d))
     }
 
-    fn docids_from_index<'a>(
-        &self,
-        index: &'a index::Index,
-    ) -> Box<dyn Iterator<Item = index::DocId> + 'a> {
-        todo!()
+    fn docids_from_index<'a>(&self, index: &'a Index) -> Box<dyn Iterator<Item = DocId> + 'a> {
+        // Get iterators from all queries
+        let iterators: Vec<_> = self
+            .queries
+            .iter()
+            .map(|q| q.docids_from_index(index))
+            .collect();
+        Box::new(ConjunctionIterator::new(iterators))
+    }
+}
+
+struct ConjunctionIterator<'a> {
+    iterators: Vec<Box<dyn Iterator<Item = DocId> + 'a>>,
+    current_docids: Vec<Option<DocId>>,
+    max_id: usize,
+    last_match: Option<DocId>,
+}
+impl<'a> ConjunctionIterator<'a> {
+    fn new(iterators: Vec<Box<dyn Iterator<Item = DocId> + 'a>>) -> Self {
+        let n_its = iterators.len();
+        ConjunctionIterator {
+            iterators,
+            current_docids: vec![None; n_its],
+            max_id: usize::MIN,
+            last_match: None,
+        }
+    }
+}
+
+impl Iterator for ConjunctionIterator<'_> {
+    type Item = DocId;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // We need to advance all iterators that are lower than the current max_id
+        let mut max_iterations = 5;
+        loop {
+            max_iterations -= 1;
+            if max_iterations == 0 {
+                panic!("Infinite loop detected in ConjunctionIterator");
+            }
+            let to_advance: Vec<usize> = self
+                .current_docids
+                .iter()
+                .enumerate()
+                .filter(|(_i, id)| {
+                    match (id, self.last_match) {
+                        (_, Some(_)) => true, // Always advance all if there was a match.
+                        (Some(id), _) => {
+                            // If we have a current ID, we only advance if it is lower than the max_id
+                            *id < self.max_id
+                        }
+                        (None, _) => true, // Always advance none.
+                    }
+                })
+                .map(|(i, _)| i)
+                .collect(); // We only care about i.
+
+            //println!("Advancing iterators: {:?}", to_advance);
+            //return None;
+
+            for i in to_advance {
+                // Change doc ids in place.
+                self.current_docids[i] = self.iterators[i].next();
+            }
+
+            //println!("Current docids: {:?}", self.current_docids);
+            //return None;
+
+            // If any is None, we are done, as there is no chance
+            // to find a common document ID later on.
+            if self.current_docids.iter().any(|id| id.is_none()) {
+                // TODO later: Maybe mark this iterator as exhausted
+                // So we dont do all the above ever again.
+                //println!("Some iterators are exhausted, returning None");
+                return None;
+            } else {
+                //println!("None of the iterators is exhausted, continuing");
+            }
+
+            //return None;
+
+            self.max_id = self
+                .current_docids
+                .iter()
+                .filter_map(|id| *id)
+                // filter_map makes sure only
+                // the Some values make it through, and they are unwrapped.
+                // Hence the Iterator<Item = usize> type
+                .max() // Vanilla max. on an iterator of usize
+                .unwrap_or(usize::MIN);
+
+            //println!("Max ID: {}", self.max_id);
+            //return None;
+
+            // If all current docids are the same, we return it
+            if self
+                .current_docids
+                .iter()
+                .all(|id| id == &Some(self.max_id))
+            {
+                //println!("All current docids are the same max, returning max_id");
+                self.last_match = Some(self.max_id);
+                return self.last_match;
+            }
+
+            // We havent returned anything, loop again
+            // until we find a common document ID or
+            // any of the iterator is exhausted.
+            self.last_match = None; // No match. Next iteration will try
+                                    // to advance up to the max_id
+        }
     }
 }
 
@@ -59,10 +159,7 @@ impl Query for DisjunctionQuery {
         self.queries.iter().any(|q| q.matches(d))
     }
 
-    fn docids_from_index<'a>(
-        &self,
-        index: &'a index::Index,
-    ) -> Box<dyn Iterator<Item = index::DocId> + 'a> {
+    fn docids_from_index<'a>(&self, _index: &'a Index) -> Box<dyn Iterator<Item = DocId> + 'a> {
         todo!()
     }
 }
@@ -84,10 +181,7 @@ impl Query for TermQuery {
         d.field_values_iter(&self.field)
             .map_or(false, |mut iter| iter.any(|value| value == &self.term))
     }
-    fn docids_from_index<'a>(
-        &self,
-        index: &'a index::Index,
-    ) -> Box<dyn Iterator<Item = index::DocId> + 'a> {
+    fn docids_from_index<'a>(&self, index: &'a Index) -> Box<dyn Iterator<Item = DocId> + 'a> {
         Box::new(index.term_iter(self.field.clone(), self.term.clone()))
     }
 }
---
+++ tests/queries_filter_tests.rs
@@ -0,0 +1,85 @@
+use mokapot::models::{
+    documents::Document,
+    index::Index,
+    queries::{ConjunctionQuery, Query, TermQuery},
+};
+
+#[test]
+fn test_term_query() {
+    let d: Document = Document::default()
+        .add_field("colour".into(), "blue".into())
+        .add_field("colour".into(), "green".into())
+        .add_field("taste".into(), "sweet".into());
+
+    let d2: Document = Document::default()
+        .add_field("colour".into(), "yellow".into())
+        .add_field("colour".into(), "green".into())
+        .add_field("taste".into(), "bitter".into());
+
+    let mut index = Index::new();
+    // A query on an empty index.
+    let q = TermQuery::new("colour".into(), "blue".into());
+    assert_eq!(q.docids_from_index(&index).count(), 0);
+
+    index.index_document(d.clone());
+    index.index_document(d2.clone());
+
+    assert!(q.matches(&d));
+    assert!(q.docids_from_index(&index).next().is_some());
+    assert_eq!(q.docids_from_index(&index).count(), 1);
+
+    let q2 = TermQuery::new("colour".into(), "green".into());
+    assert!(q2.matches(&d));
+    assert!(q2.matches(&d2));
+    assert_eq!(q2.docs_from_index(&index).count(), 2);
+
+    let q2 = TermQuery::new("colour".into(), "red".into());
+    assert!(!q2.matches(&d));
+    assert!(q2.docids_from_index(&index).next().is_none());
+    assert_eq!(q2.docids_from_index(&index).count(), 0);
+
+    let q3 = TermQuery::new("another_key".into(), "sausage".into());
+    assert!(!q3.matches(&d));
+    assert!(q3.docids_from_index(&index).next().is_none());
+}
+
+#[test]
+fn test_conjunction_query() {
+    let d: Document = Document::default()
+        .add_field("colour".into(), "blue".into())
+        .add_field("taste".into(), "sweet".into());
+
+    let d1: Document = Document::default()
+        .add_field("colour".into(), "yellow".into())
+        .add_field("taste".into(), "sweet".into());
+
+    let d2: Document = Document::default()
+        .add_field("colour".into(), "blue".into())
+        .add_field("taste".into(), "bitter".into());
+
+    let d3: Document = Document::default()
+        .add_field("colour".into(), "blue".into())
+        .add_field("taste".into(), "sweet".into());
+
+    let q = TermQuery::new("colour".into(), "blue".into());
+    let q2 = TermQuery::new("taste".into(), "sweet".into());
+    let conjunction_query = ConjunctionQuery::new(vec![Box::new(q), Box::new(q2)]);
+
+    assert!(conjunction_query.matches(&d));
+
+    // Index the document
+    let mut index = Index::new();
+    let doc_ids: Vec<_> = conjunction_query.docids_from_index(&index).collect();
+    assert_eq!(doc_ids, vec![]);
+
+    index.index_document(d.clone());
+    index.index_document(d1.clone());
+    index.index_document(d2.clone());
+    index.index_document(d3.clone());
+
+    let mut doc_ids = conjunction_query.docids_from_index(&index);
+    assert_eq!(doc_ids.next(), Some(0));
+    assert_eq!(doc_ids.next(), Some(3));
+    assert_eq!(doc_ids.next(), None);
+    assert_eq!(doc_ids.next(), None);
+}
