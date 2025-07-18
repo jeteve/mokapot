a380f00f1080d1ea23537674320effca079c4a32
Jerome Eteve <jerome.eteve@gmail.com>
2025-07-18 21:04:14 +0000

    Refactored out term query

--- src/models/queries.rs
+++ src/models/queries.rs
@@ -3,10 +3,14 @@ use itertools::Itertools;
 use crate::models::index::{DocId, Index};
 
 use super::documents::Document;
-use std::rc::Rc;
 
+// Submodules.
 mod query;
+mod term;
+
+// Re-export
 pub use crate::models::queries::query::*;
+pub use crate::models::queries::term::*;
 
 trait MyIterators: Iterator {
     fn schwartzian<F, K, O>(self, fk: F, ord: O) -> impl Iterator<Item = <Self as Iterator>::Item>
@@ -295,42 +299,5 @@ impl Iterator for DisjunctionIterator<'_> {
                 return Some(candidate);
             }
         }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct TermQuery {
-    field: Rc<str>,
-    term: Rc<str>,
-}
-
-impl TermQuery {
-    pub fn new(field: Rc<str>, term: Rc<str>) -> Self {
-        TermQuery { field, term }
-    }
-}
-
-impl Query for TermQuery {
-    fn doc_enrichers(&self) -> Vec<DocPredicate> {
-        vec![DocPredicate {
-            name: "{self.field}_is_{self.term}".into(),
-            query: self,
-        }]
-    }
-
-    fn matches(&self, d: &Document) -> bool {
-        d.field_values_iter(&self.field)
-            .map_or(false, |mut iter| iter.any(|value| value == &self.term))
-    }
-    fn docids_from_index<'a>(&self, index: &'a Index) -> Box<dyn Iterator<Item = DocId> + 'a> {
-        Box::new(index.term_iter(self.field.clone(), self.term.clone()))
-    }
-
-    fn to_document(&self) -> Document {
-        Document::default().with_value(self.field.clone(), self.term.clone())
-    }
-
-    fn specificity(&self) -> f64 {
-        1.0
     }
 }
---
+++ src/models/queries/term.rs
@@ -0,0 +1,41 @@
+use crate::models::documents::Document;
+use crate::models::index::*;
+use crate::models::queries::query::*;
+use std::rc::Rc;
+
+#[derive(Debug, Clone)]
+pub struct TermQuery {
+    field: Rc<str>,
+    term: Rc<str>,
+}
+
+impl TermQuery {
+    pub fn new(field: Rc<str>, term: Rc<str>) -> Self {
+        TermQuery { field, term }
+    }
+}
+
+impl Query for TermQuery {
+    fn doc_enrichers(&self) -> Vec<DocPredicate> {
+        vec![DocPredicate {
+            name: "{self.field}_is_{self.term}".into(),
+            query: self,
+        }]
+    }
+
+    fn matches(&self, d: &Document) -> bool {
+        d.field_values_iter(&self.field)
+            .map_or(false, |mut iter| iter.any(|value| value == &self.term))
+    }
+    fn docids_from_index<'a>(&self, index: &'a Index) -> Box<dyn Iterator<Item = DocId> + 'a> {
+        Box::new(index.term_iter(self.field.clone(), self.term.clone()))
+    }
+
+    fn to_document(&self) -> Document {
+        Document::default().with_value(self.field.clone(), self.term.clone())
+    }
+
+    fn specificity(&self) -> f64 {
+        1.0
+    }
+}
