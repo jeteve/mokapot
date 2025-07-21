7d12d8912ad2b631b285b2eb3b8df71d6d45162b
Jerome Eteve <jerome.eteve@gmail.com>
2025-07-21 21:33:48 +0000

    Getting started with property testing

---
+++ tests/percolator_property.rs
@@ -0,0 +1,15 @@
+use proptest::prelude::*;
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[test]
+proptest! {
+    #[test]
+    fn test_add(a in 0..1000i32, b in 0..1000i32) {
+        let sum = add(a, b);
+        assert!(sum >= a);
+        assert!(sum >= b);
+    }
+}
