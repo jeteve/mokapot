beb111fd57ca621a7d11a13095dc892f24be14b0
Jerome Eteve <jerome.eteve@gmail.com>
2025-08-14 21:45:53 +0000

    Removed comment

--- src/models/queries/termdisjunction.rs
+++ src/models/queries/termdisjunction.rs
@@ -70,6 +70,8 @@ where
     type Item = DocId;
 
     fn next(&mut self) -> Option<Self::Item> {
+        // More or less the same algorithm as the general DisjunctionIterator,
+        // but here theres is no ordering guarantee.
         // optimisation for queries containing just one term query.
         if self.iterators.len() == 1 {
             return self.iterators[0].next();
@@ -97,64 +99,3 @@ where
         }
     }
 }
-
-/* impl Iterator for DisjunctionIterator<'_> {
-    type Item = DocId;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        // Ideas for the iterator:
-        // Maintain a set of seen docIds
-        loop {
-            // This would be made empty at the beginning
-            // or when the current_docids buffer has been poped enough.
-            if self.current_docids.is_empty() {
-                // If we have no current docids, we need to advance all iterators
-                for it in self.iterators.iter_mut() {
-                    if let Some(doc_id) = it.next() {
-                        self.current_docids.push(doc_id);
-                    }
-                }
-
-                if self.current_docids.is_empty() {
-                    // If we have no current docids despite
-                    // just having tried to populate, we are done for good.
-                    return None;
-                }
-
-                // Current docids is Not empty
-
-                // Go from lower to higher doc IDs.
-                // It is important to preserve the order.
-                self.current_docids.sort();
-                self.current_docids.reverse();
-                // Cleanup seen from anything lower than min
-                let &new_min = self
-                    .current_docids
-                    .last()
-                    .expect("current_docids should not be empty here");
-                // Only keep stuff greater or equal than this new min
-                // Note that if we have seen the new_min, we need to keep
-                // is as seen. So we retain it too.
-                self.seen.retain(|&e| e >= new_min);
-            }
-
-            // This will return all the matching doc IDs,
-            // with potential duplicates.
-
-            // current_docids is not empty here.
-            let candidate = self
-                .current_docids
-                .pop()
-                .expect("current_docids should not be empty");
-
-            if !self.seen.contains(&candidate) {
-                // Candidate has not been seen.
-                // It is a good one!
-                self.seen.insert(candidate);
-                return Some(candidate);
-            }
-        }
-    }
-}
-
-*/
