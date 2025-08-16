f9ccedc54cd8c411fe6a1454ffa41bcec875151b
Jerome Eteve <jerome.eteve@gmail.com>
2025-08-16 09:34:44 +0000

    Only use one implementation of disjunction operator

--- src/models/queries/disjunction.rs
+++ src/models/queries/disjunction.rs
@@ -2,6 +2,7 @@ use super::query::*;
 use crate::models::cnf::CNFQuery;
 use crate::models::documents::Document;
 use crate::models::index::{DocId, Index};
+use crate::models::iterators;
 
 #[derive(Debug)]
 pub struct DisjunctionQuery {
@@ -24,7 +25,7 @@ impl Query for DisjunctionQuery {
             .iter()
             .map(|q| q.docids_from_index(index))
             .collect();
-        Box::new(DisjunctionIterator::new(iterators))
+        Box::new(iterators::DisjunctionIterator::new(iterators))
     }
 
     fn to_document(&self) -> Document {
@@ -53,82 +54,5 @@ impl Query for DisjunctionQuery {
 
     fn to_cnf(&self) -> CNFQuery {
         CNFQuery::from_or(self.queries.iter().map(|q| q.to_cnf()).collect())
-    }
-}
-
-struct DisjunctionIterator<'a> {
-    iterators: Vec<Box<dyn Iterator<Item = DocId> + 'a>>,
-    seen: std::collections::HashSet<DocId>,
-    current_docids: Vec<DocId>,
-}
-
-impl<'a> DisjunctionIterator<'a> {
-    fn new(iterators: Vec<Box<dyn Iterator<Item = DocId> + 'a>>) -> Self {
-        let n_its = iterators.len();
-        let docids = Vec::with_capacity(n_its);
-        DisjunctionIterator {
-            iterators,
-            seen: std::collections::HashSet::with_capacity(n_its * 2),
-            current_docids: docids,
-        }
-    }
-}
-
-impl Iterator for DisjunctionIterator<'_> {
-    type Item = DocId;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        // Ideas for the iterator:
-        // Maintain a set of seen docIds
-        loop {
-            // This would be made empty at the beginning
-            // or when the current_docids buffer has been poped enough.
-            if self.current_docids.is_empty() {
-                // If we have no current docids, we need to advance all iterators
-                for it in self.iterators.iter_mut() {
-                    if let Some(doc_id) = it.next() {
-                        self.current_docids.push(doc_id);
-                    }
-                }
-
-                if self.current_docids.is_empty() {
-                    // If we have no current docids despite
-                    // just having tried to populate, we are done for good.
-                    return None;
-                }
-
-                // Current docids is Not empty
-
-                // Go from lower to higher doc IDs.
-                // It is important to preserve the order.
-                self.current_docids.sort();
-                self.current_docids.reverse();
-                // Cleanup seen from anything lower than umin
-                let &new_min = self
-                    .current_docids
-                    .last()
-                    .expect("current_docids should not be empty here");
-                // Only keep stuff greater or equal than this new min
-                // Note that if we have seen the new_min, we need to keep
-                // is as seen. So we retain it too.
-                self.seen.retain(|&e| e >= new_min);
-            }
-
-            // This will return all the matching doc IDs,
-            // with potential duplicates.
-
-            // current_docids is not empty here.
-            let candidate = self
-                .current_docids
-                .pop()
-                .expect("current_docids should not be empty");
-
-            if !self.seen.contains(&candidate) {
-                // Candidate has not been seen.
-                // It is a good one!
-                self.seen.insert(candidate);
-                return Some(candidate);
-            }
-        }
     }
 }
