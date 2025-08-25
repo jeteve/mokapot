86d9abd95a264be4a7d8747e9b84e6e702bb30df
Jerome Eteve <jerome.eteve@gmail.com>
2025-08-25 15:22:17 +0000

    WIP real benchmark, inspired by percolator_extensive in tests

---
+++ benches/percolate_real.rs
@@ -0,0 +1,86 @@
+use std::rc::Rc;
+
+use criterion::{black_box, Throughput};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+
+use fake::faker::address::en::CountryName;
+use fake::Fake;
+use mokapot::models::percolator::{MultiPercolator, SimplePercolator};
+use mokapot::models::queries::ConjunctionQuery;
+use mokapot::models::{documents::Document, percolator::Percolator, queries::TermQuery};
+
+const FIELD: &str = "city";
+const FIELD2: &str = "taste";
+const THIRD_FIELDS: [&str; 4] = ["price", "colour", "size", "temperature"];
+
+static TASTE_VALUES: [&str; 5] = ["acidic", "sweet", "bitter", "umani", "salty"];
+
+fn one_random_data<T: Clone, const N: usize>(d: &[T; N]) -> T {
+    d[(0..d.len()).fake::<usize>()].clone()
+}
+
+fn build_query(n: usize) -> ConjunctionQuery {
+    let q1 = TermQuery::new(FIELD.into(), CountryName().fake::<String>().into());
+    let q1b = TermQuery::new(FIELD2.into(), one_random_data(&TASTE_VALUES).into());
+    //let q1 = TermQuery::new(FIELD.into(), format!("value{n}").into());
+    // Only 4 values for this one.
+    let q2 = TermQuery::new(FIELD2.into(), format!("value{}", n % 4).into());
+    ConjunctionQuery::new(vec![Box::new(q1), Box::new(q1b)])
+}
+
+fn build_percolator<P>(n: usize) -> P
+where
+    P: Percolator + std::fmt::Display + Default,
+{
+    let mut p = P::default();
+    (0..n).map(build_query).for_each(|q| {
+        p.add_query(Rc::new(q));
+    });
+    p
+}
+
+fn percolate_real(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Reallife_matching");
+
+    let third_fields = todo!();
+
+    for nqueries in [100, 10000, 50000] {
+        group.throughput(Throughput::Elements(1));
+
+        // Build percolators with n queries field=valueN
+        let p = build_percolator::<SimplePercolator>(nqueries);
+        let mp = build_percolator::<MultiPercolator>(nqueries);
+        //let h = build_hashmap(nqueries);
+
+        fn build_document(nqueries: usize) -> Document {
+            let mid_value: Rc<str> = format!("value{}", nqueries / 10).into();
+            let second_value: Rc<str> = format!("value{}", nqueries / 10 % 4).into();
+            Document::new()
+                // Find the first decile value.
+                .with_value("field", mid_value.clone())
+                .with_value("second_field", second_value)
+        }
+
+        let input_size = criterion::BatchSize::SmallInput;
+
+        group.bench_function(BenchmarkId::new("perc_dyna", &p), |b| {
+            b.iter_batched(
+                || build_document(nqueries),
+                |d| black_box(p.qids_from_document(&d).next().unwrap()),
+                input_size,
+            )
+        });
+
+        group.bench_function(BenchmarkId::new("multi_perc", &mp), |b| {
+            b.iter_batched(
+                || build_document(nqueries),
+                |d| black_box(mp.bs_qids_from_document(&d).next().unwrap()),
+                input_size,
+            )
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, percolate_real);
+criterion_main!(benches);
