187ac4b01b937b715541418d9dd1935b6738f640
Jerome Eteve <jerome.eteve@gmail.com>
2025-09-02 19:40:44 +0000

    Fix random data generation

--- benches/percolate_real.rs
+++ benches/percolate_real.rs
@@ -9,6 +9,8 @@ use fake::Fake;
 use mokapot::models::percolator::{MultiPercolator, SimplePercolator};
 use mokapot::models::queries::ConjunctionQuery;
 use mokapot::models::{documents::Document, percolator::Percolator, queries::TermQuery};
+use rand::rngs::StdRng;
+use rand::{Rng, SeedableRng};
 
 const FIELD: &str = "city";
 const FIELD2: &str = "taste";
@@ -16,36 +18,62 @@ const THIRD_FIELDS: [&str; 4] = ["price", "colour", "size", "temperature"];
 
 static TASTE_VALUES: [&str; 5] = ["acidic", "sweet", "bitter", "umani", "salty"];
 
-fn one_random_data<T: Clone, const N: usize>(d: &[T; N]) -> T {
-    d[(0..N).fake::<usize>()].clone()
+fn one_random_data<T: Clone, R: Rng + ?Sized, const N: usize>(d: &[T; N], rng: &mut R) -> T {
+    d[(0..N).fake_with_rng::<usize, _>(rng)].clone()
 }
 
-fn one_random_from_vec<T: Clone>(d: &Vec<T>) -> T {
-    d[(0..d.len()).fake::<usize>()].clone()
+fn one_random_from_vec<T: Clone, R: Rng + ?Sized>(d: &[T], rng: &mut R) -> T {
+    d[(0..d.len()).fake_with_rng::<usize, _>(rng)].clone()
 }
 
-fn build_query(_n: usize, third_fields: &HashMap<&str, Vec<String>>) -> ConjunctionQuery {
-    let q1 = TermQuery::new(FIELD.into(), CountryName().fake::<String>().into());
-    let q1b = TermQuery::new(FIELD2.into(), one_random_data(&TASTE_VALUES).into());
+fn build_query<R: Rng + ?Sized>(
+    _n: usize,
+    third_fields: &HashMap<&str, Vec<String>>,
+    rng: &mut R,
+) -> ConjunctionQuery {
+    let q1 = TermQuery::new(
+        FIELD.into(),
+        CountryName().fake_with_rng::<String, _>(rng).into(),
+    );
+    let q1b = TermQuery::new(FIELD2.into(), one_random_data(&TASTE_VALUES, rng).into());
     //let q1 = TermQuery::new(FIELD.into(), format!("value{n}").into());
     // Only 4 values for this one.
     //let q2 = TermQuery::new(FIELD2.into(), format!("value{}", n % 4).into());
-    let q3_field = one_random_data(&THIRD_FIELDS);
-    let q3_value = one_random_from_vec(third_fields.get(q3_field).unwrap());
+    let q3_field = one_random_data(&THIRD_FIELDS, rng);
+    let q3_value = one_random_from_vec(third_fields.get(q3_field).unwrap(), rng);
     let q3 = TermQuery::new(q3_field.into(), q3_value.into());
 
     ConjunctionQuery::new(vec![Box::new(q1), Box::new(q1b), Box::new(q3)])
 }
 
-fn build_percolator<P>(n: usize, third_fields: &HashMap<&str, Vec<String>>) -> P
+fn build_percolator<P, R: Rng + ?Sized>(
+    n: usize,
+    third_fields: &HashMap<&str, Vec<String>>,
+    rng: &mut R,
+) -> P
 where
     P: Percolator + std::fmt::Display + Default,
 {
     let mut p = P::default();
-    (0..n).map(|n| build_query(n, third_fields)).for_each(|q| {
-        p.add_query(Rc::new(q));
-    });
+    (0..n)
+        .map(|n| build_query(n, third_fields, rng))
+        .for_each(|q| {
+            p.add_query(Rc::new(q));
+        });
     p
+}
+
+fn build_document<R: Rng + ?Sized>(
+    _n: usize,
+    third_fields: &HashMap<&str, Vec<String>>,
+    rng: &mut R,
+) -> Document {
+    let d = Document::new()
+        .with_value(FIELD, CountryName().fake_with_rng::<String, _>(rng))
+        .with_value(FIELD2, one_random_data(&TASTE_VALUES, rng));
+    let q3_field = one_random_data(&THIRD_FIELDS, rng);
+    let q3_value = one_random_from_vec(third_fields.get(q3_field).unwrap(), rng);
+    d.with_value(q3_field, q3_value)
 }
 
 fn percolate_real(c: &mut Criterion) {
@@ -81,27 +109,20 @@ fn percolate_real(c: &mut Criterion) {
         ),
     ]);
 
-    for nqueries in [100, 10000, 50000] {
+    for nqueries in [10000, 50000] {
         group.throughput(Throughput::Elements(1));
 
-        // Build percolators with n queries field=valueN
-        let p = build_percolator::<SimplePercolator>(nqueries, &third_fields);
-        let mp = build_percolator::<MultiPercolator>(nqueries, &third_fields);
+        let mut rng = StdRng::seed_from_u64(42);
 
-        fn build_document(_n: usize, third_fields: &HashMap<&str, Vec<String>>) -> Document {
-            let d = Document::new()
-                .with_value(FIELD, CountryName().fake::<String>())
-                .with_value(FIELD2, one_random_data(&TASTE_VALUES));
-            let q3_field = one_random_data(&THIRD_FIELDS);
-            let q3_value = one_random_from_vec(third_fields.get(q3_field).unwrap());
-            d.with_value(q3_field, q3_value)
-        }
+        // Build percolators with n queries field=valueN
+        let p = build_percolator::<SimplePercolator, StdRng>(nqueries, &third_fields, &mut rng);
+        let mp = build_percolator::<MultiPercolator, StdRng>(nqueries, &third_fields, &mut rng);
 
         let input_size = criterion::BatchSize::SmallInput;
 
         group.bench_function(BenchmarkId::new("perc_dyna", &p), |b| {
             b.iter_batched(
-                || build_document(nqueries, &third_fields),
+                || build_document(nqueries, &third_fields, &mut rng),
                 |d| black_box(p.qids_from_document(&d).next()),
                 input_size,
             )
@@ -109,8 +130,9 @@ fn percolate_real(c: &mut Criterion) {
 
         group.bench_function(BenchmarkId::new("multi_perc", &mp), |b| {
             b.iter_batched(
-                || build_document(nqueries, &third_fields),
+                || build_document(nqueries, &third_fields, &mut rng),
                 |d| black_box(mp.bs_qids_from_document(&d).next()),
+                //|d| black_box(mp.qids_from_document(&d).next()),
                 input_size,
             )
         });
