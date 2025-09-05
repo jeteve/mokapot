a7a7f4589ec1d473a6f3bdd24814ef78a582b234
Jerome Eteve <jerome.eteve@gmail.com>
2025-09-05 21:38:23 +0000

    Simple function

--- src/models/queries/term.rs
+++ src/models/queries/term.rs
@@ -35,11 +35,7 @@ impl TermQuery {
     // Specialized method. Cannot be part of a trait for use of lifetime
     // in the concrete impl implementation.
     pub fn dids_from_idx<'a>(&self, index: &'a Index) -> impl Iterator<Item = DocId> + use<'a> {
-        //index.term_iter(self.field.clone(), self.term.clone())
-        index
-            .term_bs(self.field.clone(), self.term.clone())
-            .iter()
-            .clone()
+        self.bs_from_idx(index).iter()
     }
 
     pub fn bs_from_idx<'a>(&self, index: &'a Index) -> &'a RoaringBitmap {
