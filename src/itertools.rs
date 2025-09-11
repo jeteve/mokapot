390e5af233c963ac236b10fad48abe04a2a9d737
Jerome Eteve <jerome.eteve@gmail.com>
2025-09-11 22:51:10 +0100

    Testing iterator extra traits

--- src/itertools.rs
+++ src/itertools.rs
@@ -30,3 +30,45 @@ pub trait InPlaceReduce: Iterator {
 
 impl<T> TheShwartz for T where T: Iterator {}
 impl<T> InPlaceReduce for T where T: Iterator {}
+
+mod test_itertools {
+
+    #[test]
+    fn test_inplace_reduce() {
+        use super::InPlaceReduce;
+
+        let sum_all = |a: &mut i32, i: &i32| *a += i;
+
+        let vs: Vec<i32> = vec![];
+        assert_eq!(vs.into_iter().reduce_inplace(sum_all), None);
+
+        let vs = vec![1];
+        assert_eq!(vs.into_iter().reduce_inplace(sum_all), Some(1));
+
+        let vs = vec![1, 2, 3];
+        assert_eq!(vs.into_iter().reduce_inplace(sum_all), Some(6));
+    }
+
+    #[test]
+    fn test_theswartz() {
+        use super::TheShwartz;
+        #[derive(Debug)]
+        struct Stuff(f64);
+        impl Stuff {
+            fn to_f64(&self) -> f64 {
+                self.0
+            }
+        }
+
+        let vs: Vec<Stuff> = vec![Stuff(3.0), Stuff(2.0), Stuff(1.0)];
+        let sorted = vs
+            .iter()
+            .schwartzian(|&v| v.to_f64(), f64::total_cmp)
+            .collect::<Vec<_>>();
+
+        assert_eq!(
+            format!("{:?}", sorted),
+            "[Stuff(1.0), Stuff(2.0), Stuff(3.0)]"
+        );
+    }
+}
