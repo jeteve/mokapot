89c3e5ac8e9161a3939a8db66ec0f1d2851e7add
Jerome Eteve <jerome.eteve@gmail.com>
2025-09-11 22:13:14 +0000

    Better example

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # About mokapot
 
-mokapot is a percolator library in Rust. Condider this alpha software.
+mokapot is a percolator library in Rust. Consider this alpha software.
 
 ## About percolators
 
@@ -15,7 +15,11 @@ document as events.
 
 - Percolator first design
 
-- Support for fuzzy queries (coming up)
+- Performance and correctness focused.
+
+- Support for any boolean queries.
+
+- Support for prefix term matching (coming up)
 
 - Support for number comparison queries (coming up)
 
@@ -26,25 +30,43 @@ document as events.
 # Example
 
 ```rust
-
 use mokapot::prelude::*;
+
+#[test]
 fn test_percolator() {
     let mut p = Percolator::default();
+
     let q: Vec<Qid> = vec![
-        p.add_query("A".has_value("a")),
-        p.add_query("A".has_value("a") | "B".has_value("b")),
-        p.add_query("A".has_value("a") & "B".has_value("b")),
+        p.add_query("A".has_value("a")),                         //0
+        p.add_query("A".has_value("a") | "B".has_value("b")),    //1
+        p.add_query("A".has_value("a") & "B".has_value("b")),    //2
+        p.add_query(!"A".has_value("a")),                        //3
+        p.add_query((!"A".has_value("a")) | "B".has_value("b")), //4
+        p.add_query(!"A".has_value("a") & "B".has_value("b")),   //5
+        p.add_query(!"A".has_value("a") & "A".has_value("a")),   //6 - should NEVER match anything.
     ];
 
     assert_eq!(
         p.percolate(&Document::default().with_value("X", "x"))
-            .collect_vec(),
-        vec![]
+            .collect::<Vec<_>>(),
+        vec![q[3], q[4]]
+    );
+
+    assert_eq!(
+        p.percolate(&Document::default().with_value("B", "b"))
+            .collect::<Vec<_>>(),
+        vec![q[1], q[3], q[4], q[5]]
+    );
+
+    assert_eq!(
+        p.percolate(&Document::default().with_value("A", "b"))
+            .collect::<Vec<_>>(),
+        vec![q[3], q[4]]
     );
 
     assert_eq!(
         p.percolate(&Document::default().with_value("A", "a"))
-            .collect_vec(),
+            .collect::<Vec<_>>(),
         vec![q[0], q[1]]
     );
 
@@ -54,8 +76,8 @@ fn test_percolator() {
                 .with_value("A", "a")
                 .with_value("B", "b")
         )
-        .collect_vec(),
-        vec![q[0], q[1], q[2]]
+        .collect::<Vec<_>>(),
+        vec![q[0], q[1], q[2], q[4]]
     );
 }
 
--- tests/prelude_test.rs
+++ tests/prelude_test.rs
@@ -1,41 +1,40 @@
-use itertools::Itertools;
 use mokapot::prelude::*;
 
 #[test]
 fn test_percolator() {
     let mut p = Percolator::default();
-    let some_q = (!"A".has_value("a")) | "B".has_value("b");
+
     let q: Vec<Qid> = vec![
-        p.add_query("A".has_value("a")),
-        p.add_query("A".has_value("a") | "B".has_value("b")),
-        p.add_query("A".has_value("a") & "B".has_value("b")),
-        p.add_query(!"A".has_value("a")),
-        // TODO: Reinstate that. But the negative matching will have to change.
-        p.add_query(some_q.clone()),
+        p.add_query("A".has_value("a")),                         //0
+        p.add_query("A".has_value("a") | "B".has_value("b")),    //1
+        p.add_query("A".has_value("a") & "B".has_value("b")),    //2
+        p.add_query(!"A".has_value("a")),                        //3
+        p.add_query((!"A".has_value("a")) | "B".has_value("b")), //4
+        p.add_query(!"A".has_value("a") & "B".has_value("b")),   //5
+        p.add_query(!"A".has_value("a") & "A".has_value("a")),   //6 - should NEVER match anything.
     ];
 
-    assert_eq!(some_q.to_string(), "(AND (OR ~A=a B=b))");
-    // Means A=a -> B=b
-    // A document with A=a MUST have B=b to match.
-    // Means we can NOT exclude the qids matching A=a in the 'negative index'
-    // A document with !A=a should match, regardless of B value or not.
-
-    // This should also match 4
     assert_eq!(
         p.percolate(&Document::default().with_value("X", "x"))
-            .collect_vec(),
+            .collect::<Vec<_>>(),
         vec![q[3], q[4]]
     );
 
     assert_eq!(
+        p.percolate(&Document::default().with_value("B", "b"))
+            .collect::<Vec<_>>(),
+        vec![q[1], q[3], q[4], q[5]]
+    );
+
+    assert_eq!(
         p.percolate(&Document::default().with_value("A", "b"))
-            .collect_vec(),
+            .collect::<Vec<_>>(),
         vec![q[3], q[4]]
     );
 
     assert_eq!(
         p.percolate(&Document::default().with_value("A", "a"))
-            .collect_vec(),
+            .collect::<Vec<_>>(),
         vec![q[0], q[1]]
     );
 
@@ -45,7 +44,7 @@ fn test_percolator() {
                 .with_value("A", "a")
                 .with_value("B", "b")
         )
-        .collect_vec(),
+        .collect::<Vec<_>>(),
         vec![q[0], q[1], q[2], q[4]]
     );
 }
