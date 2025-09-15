1eb5e8a882721a5fca7e5ba3ec48649cb7bd81e1
Jerome Eteve <jeteve@users.noreply.github.com>
2025-09-15 12:21:43 +0000

    Bit less warnings

--- src/models/queries/common.rs
+++ src/models/queries/common.rs
@@ -1,34 +1,7 @@
-use std::rc::Rc;
-
 use crate::models::document::Document;
 
 pub type PreHeatFn = Box<dyn Fn(Document) -> Document>;
 
-pub struct PreHeater {
-    id: Rc<str>,
-    preheat: PreHeatFn,
-}
-
-impl PreHeater {
-    pub fn new<T: Into<Rc<str>>>(id: T, preheat: PreHeatFn) -> Self {
-        Self {
-            id: id.into(),
-            preheat,
-        }
-    }
-}
-
 pub trait Query {
     fn matches(&self, d: &Document) -> bool;
-}
-
-mod test {
-    use crate::{models::queries::PreHeater, prelude::Document};
-
-    #[test]
-    fn test_preheater() {
-        let ph = PreHeater::new("identity", Box::new(|d| d));
-
-        assert!((ph.preheat)(Document::default()) == Document::default());
-    }
 }
