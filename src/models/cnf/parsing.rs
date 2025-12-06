cf3bf70b40a013ad382239fabaa2b0ff4f21ab53
Jerome Eteve <jerome.eteve@gmail.com>
2025-12-06 20:23:04 +0000

    Rename AST types for more clarity

--- src/models/cnf/parsing.rs
+++ src/models/cnf/parsing.rs
@@ -22,47 +22,47 @@ impl std::str::FromStr for cnf::Query {
 }
 
 #[derive(Debug, PartialEq, Clone)]
-enum Query {
-    Neg(Box<Query>),
-    Atom(String, Operator, FieldValue),
-    And(Box<Query>, Box<Query>),
-    Or(Box<Query>, Box<Query>),
+enum QueryAST {
+    Neg(Box<QueryAST>),
+    Atom(String, OperatorAST, FieldValueAST),
+    And(Box<QueryAST>, Box<QueryAST>),
+    Or(Box<QueryAST>, Box<QueryAST>),
 }
 
-fn atom_to_cnf(field: &str, operator: &Operator, field_value: &FieldValue) -> cnf::Query {
+fn atom_to_cnf(field: &str, operator: &OperatorAST, field_value: &FieldValueAST) -> cnf::Query {
     match (&operator, &field_value) {
         // A prefix ALWAYS give a prefix, regardless of operator used.
         // It is a bit dirty, but will fix in the future.
-        (Operator::H3Inside, FieldValue::Term(t)) => 
+        (OperatorAST::H3Inside, FieldValueAST::Term(t)) => 
          t.parse::<CellIndex>().map_or_else(|_err| field.has_value(t.clone()), |ci| field.h3in(ci))
 ,
         // Cannot do H3 on integers..
-        (Operator::H3Inside, FieldValue::Integer(i)) => field.has_value(i.to_string()),
-        (_, FieldValue::Prefix(p)) => field.has_prefix(p.clone()),
-        (_, FieldValue::Term(t)) => field.has_value(t.clone()),
+        (OperatorAST::H3Inside, FieldValueAST::Integer(i)) => field.has_value(i.to_string()),
+        (_, FieldValueAST::Prefix(p)) => field.has_prefix(p.clone()),
+        (_, FieldValueAST::Term(t)) => field.has_value(t.clone()),
         // Fallback to term style query in case there is ':123'
-        (Operator::Colon, FieldValue::Integer(i)) => field.has_value(i.to_string()),
-        (Operator::Lt, FieldValue::Integer(i)) => field.i64_lt(*i),
-        (Operator::Le, FieldValue::Integer(i)) => field.i64_le(*i),
-        (Operator::Eq, FieldValue::Integer(i)) => field.i64_eq(*i),
-        (Operator::Ge, FieldValue::Integer(i)) => field.i64_ge(*i),
-        (Operator::Gt, FieldValue::Integer(i)) => field.i64_gt(*i),
+        (OperatorAST::Colon, FieldValueAST::Integer(i)) => field.has_value(i.to_string()),
+        (OperatorAST::Lt, FieldValueAST::Integer(i)) => field.i64_lt(*i),
+        (OperatorAST::Le, FieldValueAST::Integer(i)) => field.i64_le(*i),
+        (OperatorAST::Eq, FieldValueAST::Integer(i)) => field.i64_eq(*i),
+        (OperatorAST::Ge, FieldValueAST::Integer(i)) => field.i64_ge(*i),
+        (OperatorAST::Gt, FieldValueAST::Integer(i)) => field.i64_gt(*i),
     }
 }
 
-impl Query {
+impl QueryAST {
     fn to_cnf(&self) -> cnf::Query {
         match &self {
-            Query::Neg(query) => !query.to_cnf(),
-            Query::Atom(field, operator, field_value) => atom_to_cnf(field, operator, field_value),
-            Query::And(query, query1) => query.to_cnf() & query1.to_cnf(),
-            Query::Or(query, query1) => query.to_cnf() | query1.to_cnf(),
+            QueryAST::Neg(query) => !query.to_cnf(),
+            QueryAST::Atom(field, operator, field_value) => atom_to_cnf(field, operator, field_value),
+            QueryAST::And(query, query1) => query.to_cnf() & query1.to_cnf(),
+            QueryAST::Or(query, query1) => query.to_cnf() | query1.to_cnf(),
         }
     }
 }
 
 #[derive(Debug, PartialEq, Clone)]
-enum Operator {
+enum OperatorAST {
     Colon,
     Lt,
     Le,
@@ -73,7 +73,7 @@ enum Operator {
 }
 
 #[derive(Debug, PartialEq, Clone)]
-enum FieldValue {
+enum FieldValueAST {
     Term(String),
     Prefix(String),
     Integer(i64),
@@ -81,7 +81,7 @@ enum FieldValue {
 
 type MyParseError<'src> = extra::Err<Rich<'src, char>>;
 
-fn query_parser<'src>() -> impl Parser<'src, &'src str, Query, MyParseError<'src>> {
+fn query_parser<'src>() -> impl Parser<'src, &'src str, QueryAST, MyParseError<'src>> {
     recursive(|expr| {
         let recursive_atom = atom_parser()
             .or(expr.delimited_by(just('('), just(')')))
@@ -90,12 +90,12 @@ fn query_parser<'src>() -> impl Parser<'src, &'src str, Query, MyParseError<'src
         let unary = text::ascii::keyword("NOT")
             .padded()
             .repeated()
-            .foldr(recursive_atom, |_op, rhs| Query::Neg(Box::new(rhs)))
+            .foldr(recursive_atom, |_op, rhs| QueryAST::Neg(Box::new(rhs)))
             .boxed();
 
         let product = unary.clone().foldl(
             text::ascii::keyword("AND")
-                .to(Query::And as fn(_, _) -> _)
+                .to(QueryAST::And as fn(_, _) -> _)
                 .then(unary)
                 .repeated(),
             |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
@@ -105,7 +105,7 @@ fn query_parser<'src>() -> impl Parser<'src, &'src str, Query, MyParseError<'src
         // is a sum of products.
         product.clone().foldl(
             text::ascii::keyword("OR")
-                .to(Query::Or as fn(_, _) -> _)
+                .to(QueryAST::Or as fn(_, _) -> _)
                 .then(product)
                 .repeated(),
             |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
@@ -114,23 +114,23 @@ fn query_parser<'src>() -> impl Parser<'src, &'src str, Query, MyParseError<'src
     .padded()
 }
 
-fn atom_parser<'src>() -> impl Parser<'src, &'src str, Query, MyParseError<'src>> {
+fn atom_parser<'src>() -> impl Parser<'src, &'src str, QueryAST, MyParseError<'src>> {
     identifier_parser()
         .then(operator_parser())
         .then(field_value_parser())
-        .map(|((s, o), v)| Query::Atom(s, o, v))
+        .map(|((s, o), v)| QueryAST::Atom(s, o, v))
         .padded()
 }
 
-fn operator_parser<'src>() -> impl Parser<'src, &'src str, Operator, MyParseError<'src>> {
+fn operator_parser<'src>() -> impl Parser<'src, &'src str, OperatorAST, MyParseError<'src>> {
     choice((
-        just(':').to(Operator::Colon),
-        just("<=").to(Operator::Le),
-        just(">=").to(Operator::Ge),
-        just('<').to(Operator::Lt),
-        just('>').to(Operator::Gt),
-        just('=').to(Operator::Eq),
-        just("H3IN").to(Operator::H3Inside),
+        just(':').to(OperatorAST::Colon),
+        just("<=").to(OperatorAST::Le),
+        just(">=").to(OperatorAST::Ge),
+        just('<').to(OperatorAST::Lt),
+        just('>').to(OperatorAST::Gt),
+        just('=').to(OperatorAST::Eq),
+        just("H3IN").to(OperatorAST::H3Inside),
     ))
     .padded()
 }
@@ -146,7 +146,7 @@ fn identifier_parser<'src>() -> impl Parser<'src, &'src str, String, MyParseErro
         .padded()
 }
 
-fn field_value_parser<'src>() -> impl Parser<'src, &'src str, FieldValue, MyParseError<'src>> {
+fn field_value_parser<'src>() -> impl Parser<'src, &'src str, FieldValueAST, MyParseError<'src>> {
     let term_char = just('\\')
         .ignore_then(any()) // After backslash, accept any character
         .or(none_of('"')); // Or any character that's not a quote, as this is meant to use into a phrase parser.
@@ -158,9 +158,9 @@ fn field_value_parser<'src>() -> impl Parser<'src, &'src str, FieldValue, MyPars
         .then(just('*').or_not())
         .map(|(t, wc)| {
             if wc.is_some() {
-                FieldValue::Prefix(t)
+                FieldValueAST::Prefix(t)
             } else {
-                FieldValue::Term(t)
+                FieldValueAST::Term(t)
             }
         });
 
@@ -172,12 +172,12 @@ fn field_value_parser<'src>() -> impl Parser<'src, &'src str, FieldValue, MyPars
         .then(just('*').or_not())
         .map(|(t, wc)| {
             if wc.is_some() {
-                FieldValue::Prefix(t) // With a wild char, this is ALWAYS a word
+                FieldValueAST::Prefix(t) // With a wild char, this is ALWAYS a word
             } else {
                 // Attempt to parse as i64. If fail, fallback to just string.
                 t.parse::<i64>()
-                    .map(FieldValue::Integer)
-                    .unwrap_or(FieldValue::Term(t))
+                    .map(FieldValueAST::Integer)
+                    .unwrap_or(FieldValueAST::Term(t))
             }
         });
 
@@ -194,28 +194,28 @@ mod tests {
 
         assert_eq!(
             p.parse("location H3IN blablabla").output(),
-            Some(&Query::Atom(
+            Some(&QueryAST::Atom(
                 "location".to_string(),
-                Operator::H3Inside,
-                FieldValue::Term("blablabla".into())
+                OperatorAST::H3Inside,
+                FieldValueAST::Term("blablabla".into())
             ))
         );
 
         assert_eq!(
             p.parse("location H3IN 1234").output(),
-            Some(&Query::Atom(
+            Some(&QueryAST::Atom(
                 "location".to_string(),
-                Operator::H3Inside,
-                FieldValue::Integer(1234)
+                OperatorAST::H3Inside,
+                FieldValueAST::Integer(1234)
             ))
         );
 
         assert_eq!(
             p.parse("name:abc").output(),
-            Some(&Query::Atom(
+            Some(&QueryAST::Atom(
                 "name".to_string(),
-                Operator::Colon,
-                FieldValue::Term("abc".into())
+                OperatorAST::Colon,
+                FieldValueAST::Term("abc".into())
             ))
         );
 
@@ -263,16 +263,16 @@ mod tests {
 
         assert_eq!(
             p.parse("name:abc AND price<=123").output(),
-            Some(&Query::And(
-                Box::new(Query::Atom(
+            Some(&QueryAST::And(
+                Box::new(QueryAST::Atom(
                     "name".to_string(),
-                    Operator::Colon,
-                    FieldValue::Term("abc".into())
+                    OperatorAST::Colon,
+                    FieldValueAST::Term("abc".into())
                 )),
-                Box::new(Query::Atom(
+                Box::new(QueryAST::Atom(
                     "price".to_string(),
-                    Operator::Le,
-                    FieldValue::Integer(123)
+                    OperatorAST::Le,
+                    FieldValueAST::Integer(123)
                 ))
             ))
         );
@@ -289,23 +289,23 @@ mod tests {
         assert_eq!(
             p.parse("name:abc AND NOT price<=123 OR colour:blue*")
                 .output(),
-            Some(&Query::Or(
-                Box::new(Query::And(
-                    Box::new(Query::Atom(
+            Some(&QueryAST::Or(
+                Box::new(QueryAST::And(
+                    Box::new(QueryAST::Atom(
                         "name".to_string(),
-                        Operator::Colon,
-                        FieldValue::Term("abc".into())
+                        OperatorAST::Colon,
+                        FieldValueAST::Term("abc".into())
                     )),
-                    Box::new(Query::Neg(Box::new(Query::Atom(
+                    Box::new(QueryAST::Neg(Box::new(QueryAST::Atom(
                         "price".to_string(),
-                        Operator::Le,
-                        FieldValue::Integer(123)
+                        OperatorAST::Le,
+                        FieldValueAST::Integer(123)
                     ))))
                 )),
-                Box::new(Query::Atom(
+                Box::new(QueryAST::Atom(
                     "colour".to_string(),
-                    Operator::Colon,
-                    FieldValue::Prefix("blue".into())
+                    OperatorAST::Colon,
+                    FieldValueAST::Prefix("blue".into())
                 ))
             ))
         );
@@ -323,22 +323,22 @@ mod tests {
         assert_eq!(
             p.parse("colour:blue* OR name:abc AND NOT price<=123")
                 .output(),
-            Some(&Query::Or(
-                Box::new(Query::Atom(
+            Some(&QueryAST::Or(
+                Box::new(QueryAST::Atom(
                     "colour".to_string(),
-                    Operator::Colon,
-                    FieldValue::Prefix("blue".into())
+                    OperatorAST::Colon,
+                    FieldValueAST::Prefix("blue".into())
                 )),
-                Box::new(Query::And(
-                    Box::new(Query::Atom(
+                Box::new(QueryAST::And(
+                    Box::new(QueryAST::Atom(
                         "name".to_string(),
-                        Operator::Colon,
-                        FieldValue::Term("abc".into())
+                        OperatorAST::Colon,
+                        FieldValueAST::Term("abc".into())
                     )),
-                    Box::new(Query::Neg(Box::new(Query::Atom(
+                    Box::new(QueryAST::Neg(Box::new(QueryAST::Atom(
                         "price".to_string(),
-                        Operator::Le,
-                        FieldValue::Integer(123)
+                        OperatorAST::Le,
+                        FieldValueAST::Integer(123)
                     ))))
                 ))
             ))
@@ -357,23 +357,23 @@ mod tests {
         assert_eq!(
             p.parse("(colour:blue* OR name:abc) AND NOT price<=123")
                 .output(),
-            Some(&Query::And(
-                Box::new(Query::Or(
-                    Box::new(Query::Atom(
+            Some(&QueryAST::And(
+                Box::new(QueryAST::Or(
+                    Box::new(QueryAST::Atom(
                         "colour".to_string(),
-                        Operator::Colon,
-                        FieldValue::Prefix("blue".into())
+                        OperatorAST::Colon,
+                        FieldValueAST::Prefix("blue".into())
                     )),
-                    Box::new(Query::Atom(
+                    Box::new(QueryAST::Atom(
                         "name".to_string(),
-                        Operator::Colon,
-                        FieldValue::Term("abc".into())
+                        OperatorAST::Colon,
+                        FieldValueAST::Term("abc".into())
                     ))
                 )),
-                Box::new(Query::Neg(Box::new(Query::Atom(
+                Box::new(QueryAST::Neg(Box::new(QueryAST::Atom(
                     "price".to_string(),
-                    Operator::Le,
-                    FieldValue::Integer(123)
+                    OperatorAST::Le,
+                    FieldValueAST::Integer(123)
                 ))))
             ))
         );
@@ -412,28 +412,28 @@ mod tests {
         let p = atom_parser();
         assert_eq!(
             p.parse("  name:abc  ").output(),
-            Some(&Query::Atom(
+            Some(&QueryAST::Atom(
                 "name".to_string(),
-                Operator::Colon,
-                FieldValue::Term("abc".to_string())
+                OperatorAST::Colon,
+                FieldValueAST::Term("abc".to_string())
             ))
         );
 
         assert_eq!(
             p.parse("  price <= 123  ").output(),
-            Some(&Query::Atom(
+            Some(&QueryAST::Atom(
                 "price".to_string(),
-                Operator::Le,
-                FieldValue::Integer(123)
+                OperatorAST::Le,
+                FieldValueAST::Integer(123)
             ))
         );
 
         assert_eq!(
             p.parse("  price<=123  ").output(),
-            Some(&Query::Atom(
+            Some(&QueryAST::Atom(
                 "price".to_string(),
-                Operator::Le,
-                FieldValue::Integer(123)
+                OperatorAST::Le,
+                FieldValueAST::Integer(123)
             ))
         );
     }
@@ -451,13 +451,13 @@ mod tests {
     #[test]
     fn test_operator_parser() {
         let p = operator_parser();
-        assert_eq!(p.parse(":").output(), Some(&Operator::Colon));
-        assert_eq!(p.parse("<").output(), Some(&Operator::Lt));
-        assert_eq!(p.parse("> ").output(), Some(&Operator::Gt));
-        assert_eq!(p.parse(" =").output(), Some(&Operator::Eq));
-        assert_eq!(p.parse("<=  ").output(), Some(&Operator::Le));
-        assert_eq!(p.parse("  >=").output(), Some(&Operator::Ge));
-        assert_eq!(p.parse(" H3IN ").output(), Some(&Operator::H3Inside));
+        assert_eq!(p.parse(":").output(), Some(&OperatorAST::Colon));
+        assert_eq!(p.parse("<").output(), Some(&OperatorAST::Lt));
+        assert_eq!(p.parse("> ").output(), Some(&OperatorAST::Gt));
+        assert_eq!(p.parse(" =").output(), Some(&OperatorAST::Eq));
+        assert_eq!(p.parse("<=  ").output(), Some(&OperatorAST::Le));
+        assert_eq!(p.parse("  >=").output(), Some(&OperatorAST::Ge));
+        assert_eq!(p.parse(" H3IN ").output(), Some(&OperatorAST::H3Inside));
     }
 
     #[test]
@@ -467,45 +467,45 @@ mod tests {
         assert_eq!(parser.parse("").output(), None);
         assert_eq!(
             parser.parse("abc").output(),
-            Some(&FieldValue::Term("abc".to_string()))
+            Some(&FieldValueAST::Term("abc".to_string()))
         );
         assert_eq!(
             parser.parse("abc*").output(),
-            Some(&FieldValue::Prefix("abc".to_string()))
+            Some(&FieldValueAST::Prefix("abc".to_string()))
         );
 
         assert_eq!(
             parser.parse("\"boudin blanc\"").output(),
-            Some(&FieldValue::Term("boudin blanc".to_string()))
+            Some(&FieldValueAST::Term("boudin blanc".to_string()))
         );
 
         assert_eq!(
             parser.parse("\"boudin \\\" blanc\"").output(),
-            Some(&FieldValue::Term("boudin \" blanc".to_string()))
+            Some(&FieldValueAST::Term("boudin \" blanc".to_string()))
         );
 
         assert_eq!(
             parser.parse("\"boudin\\* \\\" blanc\"*").output(),
-            Some(&FieldValue::Prefix("boudin* \" blanc".to_string()))
+            Some(&FieldValueAST::Prefix("boudin* \" blanc".to_string()))
         );
 
         assert!(parser.parse("\"boudin blanc").has_errors());
 
         assert_eq!(
             parser.parse("\"123\"").output(),
-            Some(&FieldValue::Term("123".to_string()))
+            Some(&FieldValueAST::Term("123".to_string()))
         );
         assert_eq!(
             parser.parse("123").output(),
-            Some(&FieldValue::Integer(123))
+            Some(&FieldValueAST::Integer(123))
         );
         assert_eq!(
             parser.parse("123*").output(),
-            Some(&FieldValue::Prefix("123".to_string()))
+            Some(&FieldValueAST::Prefix("123".to_string()))
         );
         assert_eq!(
             parser.parse("-123abc").output(),
-            Some(&FieldValue::Term("-123abc".to_string()))
+            Some(&FieldValueAST::Term("-123abc".to_string()))
         );
     }
 }
